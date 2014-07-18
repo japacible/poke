160e7f58936eab198ced7594c9f03988107436c3
japacible <japacible@gmail.com>
2014-07-18 11:31:39 -0700

    reloading

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var poke = function(){
+var poke = function() {
   var pokeables = $('#content a:contains("Poke Back")');
 
   if (pokeables.length > 0) {
     console.log("Look, people to poke back!");
     for (var i = 0, max = pokeables.length; i < max; i++) {
-      console.log("Pokesables #" + i);
+      console.log("Pokeables #" + i);
       pokeables[i].click();
     }
   } else {
@@ -12,4 +12,10 @@ var poke = function(){
   }
 }
 
+var reload = function() {
+  console.log("Ctrl + R");
+  location.reload();
+}
+
 setInterval(poke, 5000);
+setInterval(reload, 25000);
