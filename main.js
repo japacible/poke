1ec5b42d6c56197075b27dad48ce8d9a141d21d6
Jennifer Apacible <japacible@gmail.com>
2014-07-23 23:47:23 -0700

    rm logging

--- main.js
+++ main.js
@@ -2,18 +2,13 @@ var poke = function() {
   var pokeables = $('#content a:contains("Poke Back")');
 
   if (pokeables.length > 0) {
-    console.log("Look, people to poke back!");
     for (var i = 0, max = pokeables.length; i < max; i++) {
-      console.log("Pokeables #" + i);
       pokeables[i].click();
     }
-  } else {
-    console.log("Nobody to poke back!");
   }
 }
 
 var reload = function() {
-  console.log("Ctrl + R");
   location.reload();
 }
 
